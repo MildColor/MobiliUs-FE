6b86da818cbadc7ed9878a22902abeff8900088a
SJW <96479626+MildColor@users.noreply.github.com>
2023-03-10 15:51:36 +0900

    Update env 설정 변경

--- app.config.js
+++ app.config.js
@@ -3,6 +3,8 @@ module.exports = {
   version: "1.0.0",
   extra: {
     apiUrl: process.env.REACT_APP_BASE_URL,
-    projectId: "5526b943-5e6e-4b5b-918c-d7ae100c11fa",
+    eas: {
+      projectId: "5526b943-5e6e-4b5b-918c-d7ae100c11fa",
+    },
   },
 };
