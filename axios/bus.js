ec8469b277dd3e26ceea51540bbb1664359f6672
SJW <96479626+MildColor@users.noreply.github.com>
2023-04-09 16:07:41 +0900

    Update 바뀐 API 설정

--- axios/bus.js
+++ axios/bus.js
@@ -2,13 +2,13 @@ import { api } from "./axiosConfig";
 
 export const busApis = {
   getBusArrival: async (stationNum) =>
-    await api.get(`/traffic/bus/arrival?stationNum=${stationNum}`),
+    await api.get(`/bus/arrival?stationNum=${stationNum}`),
 
   getSearchBusStation: async (stationName) =>
-    await api.get(`/traffic/search/busStation?keyword=${stationName}`),
+    await api.get(`/bus/search/busStation?station=${stationName}`),
 
   getNearbyBusStation: async ({ longitude, latitude, distance }) =>
     await api.get(
-      `/traffic/nearby/busStation?x=${longitude}&y=${latitude}&distance=${distance}`
+      `/bus/nearby/busStation?x=${longitude}&y=${latitude}&distance=${distance}`
     ),
 };
